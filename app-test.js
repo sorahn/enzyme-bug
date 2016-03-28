77ac1e3c1284ccaf44f3cc14f1a8f2b5e98fd3e1
Daryl Roberts <daryl.roberts@gmail.com>
2016-03-28 02:22:07 -0700

    add another failing case

--- app-test.js
+++ app-test.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import { mount, shallow } from 'enzyme'
+import { I18nextProvider } from 'react-i18next'
 
 import { App } from './components'
 import i18n from './i18next'
@@ -9,5 +10,9 @@ const context = { i18n }
 describe('<App />', () => {
   it('should shallow render', () => {
     shallow(<App />, { context }).html()
+  })
+
+  it('doesn\'t work this way either', () => {
+    shallow(<I18nextProvider i18n={i18n}><App /></I18nextProvider>).html()
   })
 })
